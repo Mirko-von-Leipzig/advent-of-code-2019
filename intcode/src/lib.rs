73336589672c91563b470113eebdb6d238a66747
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2019-12-17 18:37:36 +0200

    feat: tests now use #[cfg(test)]
    We have also now moved to using workspaces

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,4 +1,3 @@
-
 enum OpCode {
     ADD = 1,
     MULTIPLY = 2,
@@ -66,20 +65,25 @@ impl std::str::FromStr for Program {
     }
 }
 
-#[test]
-fn test_execute() {
-    let test_program = |input: &str, output: Vec<usize>| {
-        let mut program = input.parse::<Program>().unwrap();
-        program.execute();
-        assert_eq!(program.int_code, output);
-    };
+#[cfg(test)]
+mod int_code {
+    use super::*;
+    
+    #[test]
+    fn test_execute() {
+        let test_program = |input: &str, output: Vec<usize>| {
+            let mut program = input.parse::<Program>().unwrap();
+            program.execute();
+            assert_eq!(program.int_code, output);
+        };
 
-    test_program("1,0,0,0,99", vec![2, 0, 0, 0, 99]);
-    test_program("2,3,0,3,99", vec![2, 3, 0, 6, 99]);
-    test_program("2,4,4,5,99,0", vec![2, 4, 4, 5, 99, 9801]);
-    test_program("1,1,1,4,99,5,6,0,99", vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
-    test_program(
-        "1,9,10,3,2,3,11,0,99,30,40,50",
-        vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
-    );
+        test_program("1,0,0,0,99", vec![2, 0, 0, 0, 99]);
+        test_program("2,3,0,3,99", vec![2, 3, 0, 6, 99]);
+        test_program("2,4,4,5,99,0", vec![2, 4, 4, 5, 99, 9801]);
+        test_program("1,1,1,4,99,5,6,0,99", vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
+        test_program(
+            "1,9,10,3,2,3,11,0,99,30,40,50",
+            vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+        );
+    }
 }
